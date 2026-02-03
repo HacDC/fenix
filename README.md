92dffd48cdad71f07bfbdbf04e8888b48dd36519
Ivan Johnson <git@IvanJohnson.net>
2026-02-03 00:41:39 -0500

    Switch to nix-based development environment

--- README.md
+++ README.md
@@ -3,3 +3,34 @@
 [HacDC's Spaceblimp VIII](https://wiki.hacdc.org/en/Projects/spaceblimp/Space-Blimp)
 
 ![Franken Fenix Prototype PCB render](/electronics/PCB.png)
+
+# Supported Development Platforms
+
+In theory it shouldn't really matter what operating system your computer
+uses. This may or may not be the case in practice:
+
+* Ubuntu: works
+
+* MacOS: unknown
+
+* Windows Subsystem for Linux: unknown
+
+* NixOS: Does not work (NixOS/nixpkgs#372653). Ivan is trying to find a
+  workaround that we can use until the proper fix is merged
+  (NixOS/nixpkgs#477552).
+
+# Setup
+
+1. If you haven't already done so, you must first install Nix. The official
+   installation guide can be found here: https://nix.dev/install-nix
+
+2. Any time you open a new terminal window, you will need to run
+   `nix develop`.
+
+# Install Firmware
+
+```shell
+cd firmware/fenix-rust
+cargo build
+cargo flash
+```
---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Spaceblimp firmware";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-25.11-small";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+    let
+      devshell = pkgs.mkShell {
+        buildInputs = [
+          pkgs.rustup
+          pkgs.espup
+        ];
+        shellHook = ''
+          if ! [ -f espidf.sh ]; then
+            echo "Installing dependencies using rustup; this might take a while..."
+            # The purpose of `--log-level warn` is to hide the info log message
+            # where `espup` tells the user to `source espidf.sh`.
+            if ! espup install -f espidf.sh -t all --log-level warn; then
+              echo "ERROR: Failed to install dependencies"
+              exit 1
+            fi
+          fi
+          if ! source espidf.sh; then
+            echo "ERROR: The espidf.sh script failed"
+            exit 1
+          fi
+        '';
+      };
+    in
+    {
+      devShells.x86_64-linux.default = devshell;
+
+      # TODO: actually build our project
+      packages.x86_64-linux.default = devshell;
+    };
+}
